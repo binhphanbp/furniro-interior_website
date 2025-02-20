0a5787355bc1ecdd5fb000df953cd678725a8970
Bình Phan <phanbinhfedev150504@gmail.com>
2025-02-20 11:24:38 +0700

    html: Add Header & Footer for all pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { defineConfig } from 'vite';
-import htmlInject from 'vite-plugin-html-inject';
-import path from 'path';
 
 export default defineConfig({
-  plugins: [htmlInject()],
   root: '.',
   publicDir: 'public',
   server: {
